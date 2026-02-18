9c5e19eb3ae0f1ff71962a5964bc977f426c147b
surajeethazari <surajeet407@gmail.com>
2026-02-18 17:11:20 +0530

    commit

--- src/middleware/sample.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const app = express();
-
-// simple custom middleware
-// app.use((req, res, next) => {
-//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//   next();
-// })
--- src/server.js
+++ src/server.js
@@ -2,20 +2,14 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import notesRoutes from "./routes/notesRoutes.js";
-import { connectDB } from "./config/db.js";
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-
 app.use(express.json()); // this middleware will parse JSON bodies: req.body
 
-app.use("/api/notes", notesRoutes);
+// app.post("/analyze", );
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
-  });
+app.listen(PORT, () => {
+  console.log("Server started on PORT:", PORT);
 });
