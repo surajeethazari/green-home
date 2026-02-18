47c306e51144a8ee524d887cd89283522be08b5c
surajeethazari <surajeet407@gmail.com>
2026-02-18 16:52:47 +0530

    commit

---
+++ src/middleware/sample.js
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+
+// simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
+// })
